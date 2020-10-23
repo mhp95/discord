4cc436e32d188dd8aff4112d789b608472312dbb
mhp95 <hosseinparastar95@gmail.com>
2020-10-23 00:55:39 -0300

    message init

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Message.css";
+import { Avatar } from "@material-ui/core";
 function Message() {
-  return <div className="message"></div>;
+  return (
+    <div className="message">
+      <Avatar />
+      <div className="message__info">
+        <h4>
+          username
+          <span className="message__timestamp">time time</span>
+        </h4>
+        <p>This is a Message</p>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
